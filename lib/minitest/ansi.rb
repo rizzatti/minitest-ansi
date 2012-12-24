086f95cb24e0f6ba60b9f7f49b1305b5ce504ac1
Zeh Rizzatti <zehrizzatti@gmail.com>
2012-12-24 02:07:44 -0300

    Colorize ., F, E and S

--- lib/minitest/ansi.rb
+++ lib/minitest/ansi.rb
@@ -1,5 +1,6 @@
 require 'minitest/ansi/version'
 require 'delegate'
+require 'ansi'
 
 module MiniTest
   class Ansi < ::SimpleDelegator
@@ -7,6 +8,21 @@ module MiniTest
 
     def initialize(stream=MiniTest::Unit.output)
       super
+    end
+
+    def print(*args)
+      case args.first
+      when '.'
+        __getobj__.print(ANSI[:green] + '.' + ANSI[:clear])
+      when 'F'
+        __getobj__.print(ANSI[:yellow] + 'F' + ANSI[:clear])
+      when 'E'
+        __getobj__.print(ANSI[:red] + 'E' + ANSI[:clear])
+      when 'S'
+        __getobj__.print(ANSI[:cyan] + 'S' + ANSI[:clear])
+      else
+        __getobj__.print(*args)
+      end
     end
 
     def use!
--- spec/lib/minitest/ansi_spec.rb
+++ spec/lib/minitest/ansi_spec.rb
@@ -1,8 +1,12 @@
 require 'spec_helper'
 require 'minitest/ansi'
+require 'stringio'
 
 module MiniTest
   describe Ansi do
+    before { @output = MiniTest::Unit.output }
+    after { MiniTest::Unit.output = @output }
+
     subject { Ansi.new }
 
     it 'has a version number' do
@@ -13,6 +17,34 @@ module MiniTest
       subject.object_id.wont_be_same_as MiniTest::Unit.output.object_id
       subject.use!
       subject.object_id.must_be_same_as MiniTest::Unit.output.object_id
+    end
+
+    describe 'printing chars' do
+      subject { Ansi.new(StringIO.new) }
+
+      it 'prints . in green' do
+        subject.print('.')
+        subject.rewind
+        subject.read.must_equal "\e[32m.\e[0m"
+      end
+
+      it 'prints F in yellow' do
+        subject.print('F')
+        subject.rewind
+        subject.read.must_equal "\e[33mF\e[0m"
+      end
+
+      it 'prints E in red' do
+        subject.print('E')
+        subject.rewind
+        subject.read.must_equal "\e[31mE\e[0m"
+      end
+
+      it 'prints S in cyan' do
+        subject.print('S')
+        subject.rewind
+        subject.read.must_equal "\e[36mS\e[0m"
+      end
     end
   end
 end
