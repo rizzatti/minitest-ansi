f3aa8ea963ab3bc576b4d2b0b1b86fe5c99674b9
Zeh Rizzatti <zehrizzatti@gmail.com>
2012-12-25 23:33:50 -0300

    Refactor spec

--- spec/lib/minitest/ansi_spec.rb
+++ spec/lib/minitest/ansi_spec.rb
@@ -7,13 +7,12 @@ module MiniTest
     before { @output = MiniTest::Unit.output }
     after { MiniTest::Unit.output = @output }
 
-    subject { Ansi.new }
-
     it 'has a version number' do
-      subject.class::VERSION.must_equal MiniTest::AnsiVersion::VERSION
+      MiniTest::Ansi::VERSION.must_equal MiniTest::AnsiVersion::VERSION
     end
 
     it 'changes minitest output with use!' do
+      subject = Ansi.new
       subject.object_id.wont_be_same_as MiniTest::Unit.output.object_id
       subject.use!
       subject.object_id.must_be_same_as MiniTest::Unit.output.object_id
@@ -22,28 +21,28 @@ module MiniTest
     describe 'printing chars' do
       subject { Ansi.new(StringIO.new) }
 
+      def check_output(input, output)
+        subject.instance_eval do
+          print input
+          rewind
+          read.must_equal output
+        end
+      end
+
       it 'prints . in green' do
-        subject.print('.')
-        subject.rewind
-        subject.read.must_equal "\e[32m.\e[0m"
+        check_output('.', "\e[32m.\e[0m")
       end
 
       it 'prints F in yellow' do
-        subject.print('F')
-        subject.rewind
-        subject.read.must_equal "\e[33mF\e[0m"
+        check_output('F', "\e[33mF\e[0m")
       end
 
       it 'prints E in red' do
-        subject.print('E')
-        subject.rewind
-        subject.read.must_equal "\e[31mE\e[0m"
+        check_output('E', "\e[31mE\e[0m")
       end
 
       it 'prints S in cyan' do
-        subject.print('S')
-        subject.rewind
-        subject.read.must_equal "\e[36mS\e[0m"
+        check_output('S', "\e[36mS\e[0m")
       end
     end
   end
