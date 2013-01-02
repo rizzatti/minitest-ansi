7f06566b487d83715c3a4295aa5166c5d3e57301
Zeh Rizzatti <zehrizzatti@gmail.com>
2013-01-02 17:40:25 -0300

    Add example to show colored output

---
+++ examples/sample_spec.rb
@@ -0,0 +1,25 @@
+require 'minitest/spec'
+require 'minitest/autorun'
+require 'minitest/ansi'
+
+MiniTest::Ansi.use!
+
+describe "Sample" do
+  10.times do
+    it "shows passing tests with green dots" do
+      true.must_equal true
+    end
+  end
+
+  it "shows failing tests with yellow Fs" do
+    true.wont_equal true
+  end
+
+  it "shows errors with red Es" do
+    raise "some error"
+  end
+
+  it "shows skipped tests with cyan Ss" do
+    skip
+  end
+end
