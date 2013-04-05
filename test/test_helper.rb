7a1c08b5033df1c265ff373b97cae29737151a78
José Otávio Rizzatti <zehrizzatti@gmail.com>
2013-04-05 03:47:48 -0400

    Fix the coverage problem

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,12 @@
 require 'rubygems' unless defined? Gem
 require 'bundler/setup'
-require 'minitest/autorun'
 require 'coveralls'
 
 Coveralls.wear!
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
+  SimpleCov::Formatter::HTMLFormatter,
+  Coveralls::SimpleCov::Formatter
+]
+SimpleCov.start
+
+require 'minitest/autorun'
